8ab69a7585e52dcffacd2b30e16b3b39f7ac7d16
Maxence Rogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2023-05-02 11:42:45 +0200

    username working

--- project.sh
+++ project.sh
@@ -2,18 +2,19 @@
 
 #awk -F ';' '{ print $1 " " $2 " " $3 " " $4 }' "accounts.csv" > parsed.txt
 
-:"
+
 #awf (data file parsing) : -F -> field separator ; NR>1 : row>1 ; $1 : column 1
-awk -F ';' 'NR>1 {print $1}' "accounts.csv" > names.txt
-awk -F ';' 'NR>1 {print $2}' "accounts.csv" > surnames.txt
-awk -F ';' 'NR>1 {print $3}' "accounts.csv" > mails.txt
-awk -F ';' 'NR>1 {print $4}' "accounts.csv" > passwords.txt
-"
+
+#awk -F ';' 'NR>1 {print $1}' "accounts.csv" > names.txt
+#awk -F ';' 'NR>1 {print $2}' "accounts.csv" > surnames.txt
+#awk -F ';' 'NR>1 {print $3}' "accounts.csv" > mails.txt
+#awk -F ';' 'NR>1 {print $4}' "accounts.csv" > passwords.txt
 
 #while read : execution a chaque ligne ; -r : text brut sans interpretation des \ par exemple
 tail -n +2 accounts.csv | while IFS=';' read -r NAME SURNAME MAIL PASSWORD; do
-    $username = $SURNAME | sed 's/(\w{1})/\1/g'
-    echo $username
+    #username="$NAME"
+    username=${NAME:0:1}$(echo "$SURNAME" | sed 's/ //g')
+    echo "$username"
     #sudo adduser 
     #echo "$NAME $SURNAME $MAIL $PASSWORD"
 done
