30e9fe63282780e22af5a84401017447cb0390cc
Maxence Rogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2023-05-15 21:33:13 +0200

    test cron

--- project.sh
+++ project.sh
@@ -26,10 +26,8 @@ do
 done
 
 
-#Variables
+# Variables
 #smtp://$usermail:$pass;auth=mech,...@host:$port;$params
-
-#declare -a userList=()
 file="accounts.csv"
 Luser="mroger25"
 Lpass="Isen44N"
@@ -43,7 +41,7 @@ smtpUrl="smtp://$usermail:$pass;auth=LOGIN@smtp.office365.com:587"
 
 #mail --subject "Ceci est un test" --exec "set sendmail=smtp://mael.grellier-neau%40isen-ouest.yncrea.fr:68Mgn04N*;auth=LOGIN@smtp.office365.com:587" --append "From:mael.grellier-neau@isen-ouest.yncrea.fr" mael.grelneau@gmail.com <<< "<body>"
 
-#Variables SSH
+# Variables SSH
 SSH_HOST="10.30.48.100"
 SSH_USER="mroger25"
 SSH_KEY="/home/$Luser/.ssh/id_rsa"
@@ -54,23 +52,16 @@ SMTP_COMMAND='mail --subject "Test" --exec "set sendmail=$smtpUrl" --append "Fro
 
 #ssh -i $SSH_KEY $SSH_USER@$SSH_HOST "echo '$SMTP_COMMAND' | nc localhost 25"
 
-#awf (data file parsing) : -F -> field separator ; NR>1 : row>1 ; $1 : column 1
-
-#awk -F ';' 'NR>1 {print $1}' "accounts.csv" > names.txt
-#awk -F ';' 'NR>1 {print $2}' "accounts.csv" > surnames.txt
-#awk -F ';' 'NR>1 {print $3}' "accounts.csv" > mails.txt
-#awk -F ';' 'NR>1 {print $4}' "accounts.csv" > passwords.txt
-
-#while read : execution a chaque ligne ; -r : text brut sans interpretation des \ par exemple
+# while read : execution a chaque ligne ; -r : text brut sans interpretation des \ par exemple
 tail -n +2 accounts.csv | while IFS=';' read -r NAME SURNAME MAIL PASSWORD; do
     username=${NAME:0:1}$(echo "$SURNAME") # prend la premiere lettre du prenom et le nom
     username=$(echo "$username" | sed -e 's/[[:space:]]//g') # supprime les ' '
     
-    #echo $username
+    # echo $username
     userList+=("$username")
 
     mdp=${PASSWORD::-2} #supprime les \n
-    #echo $mdp
+    # echo $mdp
 
     if [ $choice == 1 ]
     then
@@ -80,40 +71,52 @@ tail -n +2 accounts.csv | while IFS=';' read -r NAME SURNAME MAIL PASSWORD; do
     if [ $choice == 2 ]
     then
 
-        useradd -m $username #-m pour créer dossier /hom auto
+        useradd -m $username #-m pour créer dossier /home auto
         
         echo -e "${PASSWORD::-2}\n${PASSWORD::-2}" | passwd "$username" #création du mdp
         chage -d 0 $username #expiration du mdp
 
-        #mkdir /home/$username/a_sauver #création d'un dossier a_sauver par user
+        # mkdir /home/$username/a_sauver #création d'un dossier a_sauver par user
 
-        #mail --subject "<subject>" --exec "set sendmail=<smtp-url>" --append "From:<sender-email>" <reciever-email> <<< "<body>"
+        # mail --subject "<subject>" --exec "set sendmail=<smtp-url>" --append "From:<sender-email>" <reciever-email> <<< "<body>"
     fi
+
+    # Sauvergarde
     
-    
-done
 
-#read -ap $userList
-#Sauvergarde
-for users in "${userList[*]}"
-do
-    echo $users
-done
+    # Définir le répertoire à sauvegarder
+    #BACKUP_DIR="/home/$username/a_sauver"
 
-for (( i=0; i < ${#tab[*]}; i++ )); do
-    echo ${tab[i]}
-done
-
-
-#for users in userList
-#do
-    #Répertoire à sauvegarder
-    #BACKUP_DIR="/home/$users/a_sauver"
-
-    #Nom et l'emplacement du fichier de sauvegarde
+    # Nom et l'emplacement du fichier de sauvegarde
     #BACKUP_FILE="$SSH_USER@$SSH_HOST:/home/saves/save_$username.tgz"
 
 
+    #(crontab -l && echo "* * * * 1-5 echo 'hello'") | crontab -
+    #0 23 * * 1-5
 
-    #ssh -i $SSH_KEY $SSH_USER@$SSH_HOST "tar -czvf - $BACKUP_DIR" > $BACKUP_FILE
-#done
+    #CronCommand="ssh -i $SSH_KEY $SSH_USER@$SSH_HOST "tar -czvf - $BACKUP_DIR" > $BACKUP_FILE"
+
+    #Répertoire à sauvegarder
+
+
+    #write out current crontab
+    #crontab -l > mycron
+    #echo new cron into cron file
+    #echo "0 23 * * 1-5 $CronCommand" >> mycron
+    #install new cron file
+    #crontab mycron
+    #rm mycron
+done
+
+#(crontab -l && echo '* * * * 1-5 touch bonjour.txt') | crontab -
+#service cron start
+
+#0 23 * * 1-5
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "* * * * * touch /home/bjr.txt" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
